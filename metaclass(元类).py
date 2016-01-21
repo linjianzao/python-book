1b9b8053d09a0b9486a977dc05db5cf846974dbd
jianzaolin <564615069@qq.com>
2016-01-21 17:15:53 +0800

    Update metaclass(元类).py

--- metaclass(元类).py
+++ metaclass(元类).py
@@ -1,1 +1,13 @@
+#!/bin/env python
+# -*- coding:utf8 -*- 
+class ListMetaclass(type):
+    def __new__(cls,name,bases,attrs):
+        attrs['add'] = lambda self,value:self.append(value)
+        return type.__new__(cls,name,bases,attrs)
 
+class Mylist(list):
+    __metaclass__ = ListMetaclass
+
+l = Mylist()
+l.add(1)
+print l
